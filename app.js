f61641088e6a9ee73b9845f8f7f2f2c5b2d2f2bb
Emily <episaygnane@me.com>
2022-08-02 12:10:59 -0700

    added const into app, started eventlisteners

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 // import functions and grab DOM elements
+const currentPollEl = document.getElementById('current-poll-container');
+const pastPollsEl = document.getElementById('past-poll-container');
 
+const optionForm = document.getElementById('option-form');
+const optionAAddButton = document.getElementById('option-a-add-button');
+const optionBAddButton = document.getElementById('option-b-add-button');
+const optionASubtractButton = document.getElementById('option-a-subtract-button');
+const optionBSubtractButton = document.getElementById('option-b-subtract-button');
+const optionAInput = document.getElementById('option-a-input');
+const optionBInput = document.getElementById('option-b-input');
 // let state
+const pastPolls = [];
+
+let currentPol = {
+    optionA: '',
+    optionB: '',
+    votesA: 0,
+    votesB: 0,
+};
+
+optionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(optionForm);
+
+    
+})
 
 // set event listeners 
   // get user input
